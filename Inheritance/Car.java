5613a86b71b042144e7c11e4e4a4177ecbbec1f4
Sahil <sahilkr904@gmail.com>
2026-01-25 13:02:10 +0530

    super keyword

--- Inheritance/Car.java
+++ Inheritance/Car.java
@@ -1,29 +1,26 @@
 public class Car extends Vehicle{
 
     String color;
+
+    Car(){
+        // super();
+        super(3); // Can only be use in constructor body
+        System.out.println("Car is being created");
+    }
     
-    // Method Overriding
     void start(){
-        // this keyword is used to take reference of that instance or object which is currently implementing
+        super.start();
         System.out.println(this);
         System.out.println(this.model+" Car is starting");
     }
      
     public static void main(String args[]){
         Car c1 = new Car();
+        System.out.println(c1.wheelcounts);
         c1.wheelcounts = 4;
         c1.model = "abcde";
         c1.color = "red";
+        System.out.println(c1.wheelcounts);
         c1.start();
-
-        Car c2 = new Car();
-        c2.model = "ghijk";
-        c2.start();
-
-        Car c3 = new Car();
-        c3 .model = "gchjeu";
-        c3.start();
-
-        // Here we can see that this keyword working as it take the reference of particular instance
     }
 }
--- Inheritance/Vehicle.java
+++ Inheritance/Vehicle.java
@@ -2,6 +2,15 @@ public class Vehicle{
     int wheelcounts;
     String model;
 
+    Vehicle(){
+        System.out.println("Creating a vehicle instance");
+    }
+
+    Vehicle(int wheelcounts){
+        this.wheelcounts = wheelcounts;
+        System.out.println("Creating vehicle with wheels");
+    }
+
     void start(){
         System.out.println("Vehicle is starting");
     }
