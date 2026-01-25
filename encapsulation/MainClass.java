6c377d692c1c86284d2409f3a6ba0a1ae25e0134
Sahil <sahilkr904@gmail.com>
2026-01-25 20:28:31 +0530

    PackageandImport

---
+++ encapsulation/MainClass.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+// import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.List;
+
+import java.util.*;
+
+public class MainClass{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+    }
+}
